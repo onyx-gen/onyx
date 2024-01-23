6b3af40e0081d7ec84228caa1957e3283cd31667
mfranken <miguelfranken90@gmail.com>
2024-01-23 21:48:59 +0100

    Simplify getAppliedTokens method

--- src/tokens.ts
+++ src/tokens.ts
@@ -7,33 +7,21 @@ export type DesignTokensObject = {
 export type DesignTokens = Map<Properties, string>
 
 /**
- * This function retrieves the applied design tokens from a given node.
- * It first gets all the keys from the Properties object.
- * Then, it maps over these keys and retrieves the shared plugin data from the node for each key.
- * If the value exists, it is added to the array.
- * Finally, it removes the first and last character from the value and returns an object with the key-value pairs.
- *
+ * Retrieves the applied design tokens from a given node.
+ * It iterates over each property in Properties, retrieves the corresponding shared plugin data from the node,
+ * and then constructs a map of these key-value pairs after slicing off the first and last character of each value.
  * @param node - The node from which to retrieve the applied design tokens.
- * @returns An object containing the applied design tokens.
+ * @returns A Map containing the applied design tokens.
  */
 export function getAppliedTokens(node: SceneNode): DesignTokens {
-  const tokenKeys = Object.keys(Properties)
-
   const tokenMap: DesignTokensObject = Object.fromEntries(
-    tokenKeys
-      .map((key) => {
-        const value = node
-          .getSharedPluginData('tokens', key)
-
-        return [key, value]
+    Object.entries(Properties)
+      .map(([key]) => {
+        const value = node.getSharedPluginData('tokens', key)
+        return value ? [key, value.slice(1, -1)] : undefined
       })
-      .filter(([, value]) => !!value)
-      .map(([key, value]) => ([key, value.slice(1, -1)])),
+      .filter(entry => entry !== undefined) as [Properties, string][],
   )
 
-  const myMap: DesignTokens = new Map()
-  for (const [key, value] of Object.entries(tokenMap))
-    myMap.set(key as Properties, value)
-
-  return myMap
+  return new Map(Object.entries(tokenMap) as [Properties, string][])
 }
