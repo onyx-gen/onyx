82be685e21a67d0323561e3e2043d9ceeb98b936
mfranken <miguelfranken90@gmail.com>
2024-01-24 09:31:09 +0100

    Compute uniquePropertiesGroupedByPropName

--- src/set/component-set-processor.ts
+++ src/set/component-set-processor.ts
@@ -1,6 +1,6 @@
 import FigmaNodeParser from '../parsers/figma-node.parser'
 import HTMLGenerator from '../generators/html.generator'
-import type { ComponentCollection, ComponentPropsWithState } from './types'
+import type { ComponentCollection, ComponentPropsWithState, SinglePropertyObject } from './types'
 import { getComponentProperties, groupComponentsByProp } from './utils'
 
 class ComponentSetProcessor {
@@ -18,10 +18,43 @@ class ComponentSetProcessor {
     }))
 
     const componentCollectionWithState: ComponentCollection<ComponentPropsWithState> = componentCollection
-      .filter(component => !!component.props.state) as ComponentCollection<ComponentPropsWithState>
+      .filter(componentData => 'state' in componentData.props) as ComponentCollection<ComponentPropsWithState>
 
     // Group components by their 'state' property.
     const componentCollectionGroupedByState = groupComponentsByProp(componentCollectionWithState, 'state')
+
+    // Extracting unique properties excluding 'state' from each component.
+    const propertyObjects: SinglePropertyObject[] = Object.values(componentCollectionGroupedByState).flatMap(group => group
+      .flatMap(({ props }) =>
+        Object.entries(props)
+          .filter(([propName]) => propName !== 'state')
+          .map(([propName, propValue]) => ({ [propName]: propValue } as SinglePropertyObject)),
+      ),
+    )
+
+    // Eliminating duplicate property objects.
+    const uniquePropertySet = new Set(propertyObjects.map(obj => JSON.stringify(obj)))
+    const uniqueProperties: SinglePropertyObject[] = Array.from(uniquePropertySet).map(str => JSON.parse(str))
+
+    // Reduce uniqueProperties to an object with keys as property names and values as arrays of those property values.
+    const uniquePropertiesGroupedByPropName = uniqueProperties.reduce<{ [key: string]: string[] }>((acc, propObj) => {
+      const propName = Object.keys(propObj)[0] as keyof SinglePropertyObject
+      const propValue = propObj[propName]
+
+      if (propValue !== undefined) {
+        const propValueArray = acc[propName] || []
+        return {
+          ...acc,
+          [propName]: [...propValueArray, propValue],
+        }
+      }
+      return acc
+    }, {})
+
+    console.log(uniquePropertiesGroupedByPropName)
+
+    // get unique objects and remove duplicates in test object
+
     console.log(componentCollectionGroupedByState)
 
     const children = node.children as ComponentNode[]
--- src/set/types.ts
+++ src/set/types.ts
@@ -17,3 +17,14 @@ export type ComponentCollection<T extends ComponentProps = ComponentProps> = {
   component: ComponentNode
   props: T
 }[]
+
+// Assuming ComponentProps is a type with string keys and string values.
+export type ExcludedStateProps = Omit<ComponentProps, 'state'>
+
+// This will represent an object with a single key-value pair,
+// which can be any of the keys from ExcludedStateProps.
+export type SinglePropertyObject = {
+  [K in keyof ExcludedStateProps]: {
+    [P in K]: ExcludedStateProps[P];
+  }
+}[keyof ExcludedStateProps]
