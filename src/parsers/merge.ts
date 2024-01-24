6d894ae3d166bf069f0965ba3313939bcda06dca
mfranken <miguelfranken90@gmail.com>
2024-01-24 11:40:41 +0100

    Add documentation to tree merger

--- src/parsers/merge.ts
+++ src/parsers/merge.ts
@@ -1,11 +1,29 @@
 import type { ContainerNodeData, IconNodeData, InstanceNodeData, TextNodeData, TreeNode, TreeNodeData } from '../interfaces'
 import { difference } from '../set/utils'
 
+/**
+ * Class responsible for merging two tree structures.
+ * The merging logic is based on the type of the nodes and their respective data.
+ */
 class TreeMerger {
+  /**
+   * Merges two TreeNode structures into one.
+   *
+   * @param tree1 The first TreeNode to merge.
+   * @param tree2 The second TreeNode to merge.
+   * @returns TreeNode representing the merged result of tree1 and tree2.
+   */
   public merge(tree1: TreeNode, tree2: TreeNode): TreeNode {
     return this.mergeNodes(tree1, tree2)
   }
 
+  /**
+   * Merges two TreeNode objects.
+   *
+   * @param node1 The first TreeNode to merge.
+   * @param node2 The second TreeNode to merge.
+   * @returns TreeNode representing the merged result of node1 and node2.
+   */
   private mergeNodes(node1: TreeNode, node2: TreeNode): TreeNode {
     // Handle the case where either of the nodes is missing
     if (!node1)
@@ -22,6 +40,13 @@ class TreeMerger {
     return { data: mergedData, children }
   }
 
+  /**
+   * Merges the data of two TreeNodeData objects.
+   *
+   * @param data1 The first TreeNodeData to merge.
+   * @param data2 The second TreeNodeData to merge.
+   * @returns TreeNodeData representing the merged result of data1 and data2.
+   */
   private mergeData(data1: TreeNodeData, data2: TreeNodeData): TreeNodeData {
     // Merge based on the type of TreeNodeData
     switch (data1.type) {
@@ -38,6 +63,13 @@ class TreeMerger {
     }
   }
 
+  /**
+   * Merges the children arrays of two TreeNode objects.
+   *
+   * @param children1 The first array of TreeNode children to merge.
+   * @param children2 The second array of TreeNode children to merge.
+   * @returns Array of TreeNode representing the merged children.
+   */
   private mergeChildren(children1: TreeNode[], children2: TreeNode[]): TreeNode[] {
     // Merge children arrays by corresponding positions
     const maxLength = Math.max(children1.length, children2.length)
@@ -48,7 +80,13 @@ class TreeMerger {
     return mergedChildren
   }
 
-  // Implement specific merging strategies for each TreeNodeData subtype
+  /**
+   * Merges ContainerNodeData objects.
+   *
+   * @param data1 The first ContainerNodeData to merge.
+   * @param data2 The second ContainerNodeData to merge.
+   * @returns ContainerNodeData representing the merged result.
+   */
   private mergeContainerData(data1: ContainerNodeData, data2: ContainerNodeData): ContainerNodeData {
     if (data1.css !== data2.css) {
       const cssDiff = difference(data1.css, data2.css)
@@ -57,7 +95,7 @@ class TreeMerger {
 
       return {
         type: 'container',
-        css: new Set([...data1.css, hoverCss]), // TODO MF: get difference and add hover variants
+        css: new Set([...data1.css, hoverCss]),
       }
     }
     else {
@@ -65,6 +103,13 @@ class TreeMerger {
     }
   }
 
+  /**
+   * Merges TextNodeData objects.
+   *
+   * @param data1 The first TextNodeData to merge.
+   * @param data2 The second TextNodeData to merge.
+   * @returns TextNodeData representing the merged result.
+   */
   private mergeTextData(data1: TextNodeData, data2: TextNodeData): TextNodeData {
     if (data1.text !== data2.text) {
       // Example: Concatenate text
@@ -78,11 +123,25 @@ class TreeMerger {
     }
   }
 
+  /**
+   * Merges InstanceNodeData objects.
+   *
+   * @param data1 The first InstanceNodeData to merge.
+   * @param data2 The second InstanceNodeData to merge.
+   * @returns InstanceNodeData representing the merged result.
+   */
   private mergeInstanceData(data1: InstanceNodeData, data2: InstanceNodeData): InstanceNodeData {
     // TODO MF
     return data1
   }
 
+  /**
+   * Merges IconNodeData objects.
+   *
+   * @param data1 The first IconNodeData to merge.
+   * @param data2 The second IconNodeData to merge.
+   * @returns IconNodeData representing the merged result.
+   */
   private mergeIconData(data1: IconNodeData, data2: IconNodeData): IconNodeData {
     // TODO MF
     return data1
