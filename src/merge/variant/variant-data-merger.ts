1b716b32fda3a74e4fd111067f89079be3122477
mfranken <miguelfranken90@gmail.com>
2024-01-29 09:44:42 +0100

    Implement simple mergeContainerData method for merging variant trees

--- src/merge/variant/variant-data-merger.ts
+++ src/merge/variant/variant-data-merger.ts
@@ -1,6 +1,7 @@
-import type { ContainerNodeData, IconNodeData, InstanceNodeData } from '../../interfaces'
+import type { ContainerNodeCSSData, ContainerNodeData, IconNodeData, InstanceNodeData } from '../../interfaces'
 import AbstractDataMerger from '../data-merger'
-import type { VariantPermutation } from '../../set/types'
+import type { VariantKey, VariantPermutation } from '../../set/types'
+import { variantKey } from '../utils'
 
 class VariantDataMerger extends AbstractDataMerger {
   constructor(private readonly variantPermutation: VariantPermutation) {
@@ -8,9 +9,19 @@ class VariantDataMerger extends AbstractDataMerger {
     super()
   }
 
+  private get variant(): VariantKey {
+    return variantKey(this.variantPermutation)
+  }
+
   protected mergeContainerData(data1: ContainerNodeData, data2: ContainerNodeData): ContainerNodeData {
-    console.error('[VariantDataMerger] mergeContainerData method not yet implemented.', { data1, data2 })
-    return data1
+    const cssData: ContainerNodeCSSData = {
+      ...data1.css,
+      ...data2.css,
+    }
+
+    // TODO MF: Do optimization here to remove duplicate CSS rules
+
+    return { type: 'container', css: cssData }
   }
 
   protected mergeInstanceData(data1: InstanceNodeData, data2: InstanceNodeData): InstanceNodeData {
