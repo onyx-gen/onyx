eacd1a2978451d2b72724a102048a6402fc94997
mfranken <miguelfranken90@gmail.com>
2024-01-29 12:48:22 +0100

    Refactoring

--- src/merge/variant/variant-data-merger.ts
+++ src/merge/variant/variant-data-merger.ts
@@ -14,61 +14,73 @@ class VariantDataMerger extends AbstractDataMerger {
     return variantKey(this.variantPermutation)
   }
 
-  protected mergeContainerData(data1: ContainerNodeData, data2: ContainerNodeData): ContainerNodeData {
+  protected mergeContainerData(
+    data1: ContainerNodeData,
+    data2: ContainerNodeData,
+  ): ContainerNodeData {
     data1.css = data1.css || {}
     data2.css = data2.css || {}
 
-    const keysData1 = Object.keys(data1.css)
-    if (keysData1.length > 0) {
-      const firstKey = keysData1.find(key => key !== 'default' && key !== this.variant)
-      // const firstKey = keysData1[0]
-
-      if (!firstKey) {
-        console.error('firstKey not found', { keysData1 })
-        return data1
-      }
-      else {
-        console.log('firstKey', firstKey)
-      }
-
-      const cssData1Variant = data1.css[firstKey]
-      const cssData2Variant = data2.css[this.variant]
-
-      if (!cssData1Variant || !cssData2Variant) {
-        console.error('cssData1Variant or cssData2Variant empty', { cssData1Variant, cssData2Variant })
-        return data1
-      }
-
-      const differenceData1Data2 = calculateVariantCSSDifference(cssData1Variant, cssData2Variant)
-      const differenceData2Data1 = calculateVariantCSSDifference(cssData2Variant, cssData1Variant)
-      const unionData1Data2 = calculateVariantCSSUnion(cssData1Variant, cssData2Variant)
-      const intersectionData1Data2 = calculateVariantCSSDifference(calculateVariantCSSDifference(unionData1Data2, differenceData1Data2), differenceData2Data1)
-
-      console.log('variant', this.variant)
-      console.log('firstKey', firstKey)
-      // cssData[`differenceData1Data2-${this.variant}`] = differenceData1Data2
-      // cssData[`differenceData2Data1-${this.variant}`] = differenceData2Data1
-      // cssData[`intersectionData1Data2-${this.variant}`] = intersectionData1Data2
-
-      const defaultCSS = data1.css.default
-      console.log('defaultCSS', defaultCSS)
-
-      const css: ContainerNodeCSSData = {
-        default: calculateVariantCSSUnion(defaultCSS, intersectionData1Data2),
-        [firstKey]: differenceData1Data2,
-        [this.variant]: differenceData2Data1,
-      }
-
-      return { type: 'container', css }
-    }
-    else {
-      console.error('data1.css is empty')
+    const firstKey = this.getFirstKey(data1.css)
+    if (!firstKey) {
+      console.error('firstKey not found', { keysData1: Object.keys(data1.css) })
+      return data1
     }
 
-    // TODO MF: Do optimization here to remove duplicate CSS rules
+    const cssData1Variant = data1.css[firstKey]
+    const cssData2Variant = data2.css[this.variant]
 
-    console.error('[VariantDataMerger] mergeContainerData handling of this case not yet implemented.', { data1, data2 })
-    return { type: 'container' }
+    if (!cssData1Variant || !cssData2Variant) {
+      console.error('cssData1Variant or cssData2Variant empty', {
+        cssData1Variant,
+        cssData2Variant,
+      })
+      return data1
+    }
+
+    const mergedCSSData = this.mergeCSSData(
+      cssData1Variant,
+      cssData2Variant,
+      data1.css.default,
+      firstKey,
+    )
+
+    return { type: 'container', css: mergedCSSData }
+  }
+
+  private getFirstKey(cssData: ContainerNodeCSSData): string | null {
+    const keys = Object.keys(cssData)
+    return keys.find(key => key !== 'default' && key !== this.variant) || null
+  }
+
+  private mergeCSSData(
+    cssData1Variant: any,
+    cssData2Variant: any,
+    defaultCSS: any,
+    firstKey: string,
+  ): ContainerNodeCSSData {
+    const differenceData1Data2 = calculateVariantCSSDifference(
+      cssData1Variant,
+      cssData2Variant,
+    )
+    const differenceData2Data1 = calculateVariantCSSDifference(
+      cssData2Variant,
+      cssData1Variant,
+    )
+    const unionData1Data2 = calculateVariantCSSUnion(
+      cssData1Variant,
+      cssData2Variant,
+    )
+    const intersectionData1Data2 = calculateVariantCSSDifference(
+      calculateVariantCSSDifference(unionData1Data2, differenceData1Data2),
+      differenceData2Data1,
+    )
+
+    return {
+      default: calculateVariantCSSUnion(defaultCSS, intersectionData1Data2),
+      [firstKey]: differenceData1Data2,
+      [this.variant]: differenceData2Data1,
+    }
   }
 
   protected mergeInstanceData(data1: InstanceNodeData, data2: InstanceNodeData): InstanceNodeData {
