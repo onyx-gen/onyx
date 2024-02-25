e8dc84fb8a4691614779686e9b7733120e3cc94b
mfranken <miguelfranken90@gmail.com>
2024-02-25 18:12:30 +0100

    Show simple UI

--- src/main.ts
+++ src/main.ts
@@ -6,55 +6,12 @@
  */
 
 // Skip over invisible nodes and their descendants inside instances for faster performance.
-import FigmaNodeParser from './parsers/figma-node.parser'
-import HTMLGenerator from './generators/html.generator'
-import { getSelectedNode } from './utils'
-import ComponentSetProcessor from './set/component-set-processor'
 
 figma.skipInvisibleInstanceChildren = true
 
-/**
- * Event listener for the 'generate' event in Figma.
- * Processes the selected node and generates HTML code.
- * The generated code is displayed in the dev tools panel
- * inside Figma.
- */
-figma.codegen.on('generate', async () => {
-  const node = getSelectedNode()
+// figma.showUI(__html__)
 
-  // Early return if no node is selected
-  if (!node)
-    return []
-
-  const parser = new FigmaNodeParser()
-  const generator = new HTMLGenerator()
-
-  let html = ''
-
-  if (node.type === 'COMPONENT_SET') {
-    const componentSetProcessor = new ComponentSetProcessor()
-
-    try {
-      html = componentSetProcessor.process(node)
-    }
-    catch (error) {
-      console.error(`[UnoCSS-Variables Plugin] Error during component set processing`, error)
-    }
-  }
-  else {
-    const tree = parser.parse(node)
-
-    if (tree)
-      html = generator.generate(tree)
-    else
-      console.error('It was not possible to generate HTML code for the selected node.')
-  }
-
-  return [
-    {
-      language: 'HTML',
-      code: html,
-      title: 'UnoCSS & Tokens Studio for Figma',
-    },
-  ]
-})
+figma.showUI(
+  '<b>Hello from Figma</b>',
+  { width: 400, height: 200, title: 'My title', position: { x: 100, y: 100 } },
+)
