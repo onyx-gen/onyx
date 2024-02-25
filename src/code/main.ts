a6d4874ea0f546fbc62a79031b98f4c5780e25f1
mfranken <miguelfranken90@gmail.com>
2024-02-25 18:45:27 +0100

    Transmit generated html to UI

--- src/code/main.ts
+++ src/code/main.ts
@@ -1,3 +1,8 @@
+import FigmaNodeParser from './parsers/figma-node.parser'
+import HTMLGenerator from './generators/html.generator'
+import { getSelectedNode } from './utils'
+import ComponentSetProcessor from './set/component-set-processor'
+
 /**
  * This plugin generates HTML code that reflects the structure and styling
  * of components in Figma. It uses the UnoCSS framework to handle styling.
@@ -12,5 +17,47 @@ figma.skipInvisibleInstanceChildren = true
 figma.showUI(__html__)
 
 figma.on('selectionchange', () => {
-  figma.ui.postMessage(figma.currentPage.selection)
+  const html = generate()
+
+  figma.ui.postMessage(html)
 })
+
+/**
+ * Event listener for the 'generate' event in Figma.
+ * Processes the selected node and generates HTML code.
+ * The generated code is displayed in the dev tools panel
+ * inside Figma.
+ */
+function generate(): string {
+  const node = getSelectedNode()
+
+  // Early return if no node is selected
+  if (!node)
+    return ''
+
+  const parser = new FigmaNodeParser()
+  const generator = new HTMLGenerator()
+
+  let html = ''
+
+  if (node.type === 'COMPONENT_SET') {
+    const componentSetProcessor = new ComponentSetProcessor()
+
+    try {
+      html = componentSetProcessor.process(node)
+    }
+    catch (error) {
+      console.error(`[UnoCSS-Variables Plugin] Error during component set processing`, error)
+    }
+  }
+  else {
+    const tree = parser.parse(node)
+
+    if (tree)
+      html = generator.generate(tree)
+    else
+      console.error('It was not possible to generate HTML code for the selected node.')
+  }
+
+  return html
+}
