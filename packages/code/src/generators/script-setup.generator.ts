3b0603502b76c7fd900537047a10bac879396829
miguelfranken <miguel.franken@rwth-aachen.de>
2024-02-27 14:01:31 +0100

    Refactor script-setup generator in code package
    The logic to generate the 'Props' interface has been overhauled. Instead of simply mapping over an array, it now reduces the permutations into a collection and creates the 'Props' from this output. This change leads to a more accurate generation of script setup interfaces.

--- packages/code/src/generators/script-setup.generator.ts
+++ packages/code/src/generators/script-setup.generator.ts
@@ -1,18 +1,45 @@
 import { createIndent } from '../utils'
 import type { VariantPermutation } from '../set/types'
-import { variantKey } from '../merge/utils'
 
+/**
+ * This class generates the `script setup` code block for the generated Vue component output.
+ */
 class ScriptSetupGenerator {
+  /**
+   * Generate the script code block.
+   *
+   * Each unique value of each key in the permutations will be a type union in the generated code.
+   *
+   * @param permutations - The set of permutations to generate TypeScript interface from.
+   * @returns - The generated script setup string.
+   */
   generate(permutations: VariantPermutation[]) {
     const indent = createIndent(2)
 
-    const variantKeys = permutations.map(permutation => variantKey(permutation))
-    const props = variantKeys.map(key => `'${key}': boolean`).join(`\n${indent}`)
+    const permutationCollection = permutations.reduce((acc, permutation) => {
+      Object.entries(permutation).forEach(([key, value]) => {
+        if (acc[key])
+          acc[key].add(value)
+        else
+          acc[key] = new Set([value])
+      })
+      return acc
+    }, {} as { [key: string]: Set<string> })
+
+    const types = Object.fromEntries(
+      Object.entries(permutationCollection)
+        .map(([key, values]) => {
+          const valuesString = Array.from(values).map(value => `'${value}'`).join(' | ')
+          return [key, valuesString]
+        }),
+    )
+
+    const interfacePropsBody = Object.entries(types).map(([key, value]) => `${key}: ${value}`).join(`\n${indent}`)
 
     const code = `
 <script setup lang="ts">
   interface Props {
-    ${props}
+    ${interfacePropsBody}
   }
 
   defineProps<Props>()
