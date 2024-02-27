eea76db29a918ac956132b641d8562b468ba75ca
miguelfranken <miguel.franken@rwth-aachen.de>
2024-02-27 14:58:56 +0100

    Add computed properties to script setup generator
    The update introduces computed properties in the `script setup` block of the Vue component output. It creates a `ComputedProperties` interface and improves the generator to construct computed properties from the permutations. This enhancement allows easier data manipulation based on the state of these properties.

--- packages/code/src/generators/script-setup.generator.ts
+++ packages/code/src/generators/script-setup.generator.ts
@@ -1,6 +1,15 @@
 import { createIndent } from '../utils'
 import type { VariantPermutation } from '../set/types'
 
+type PermutationKey = string
+type PermutationValue = string
+type PermutationComputedPropertyName = string
+interface ComputedProperties {
+  [p: PermutationKey]: {
+    [p: PermutationValue]: PermutationComputedPropertyName
+  }
+}
+
 /**
  * This class generates the `script setup` code block for the generated Vue component output.
  */
@@ -28,21 +37,59 @@ class ScriptSetupGenerator {
 
     const types = Object.fromEntries(
       Object.entries(permutationCollection)
-        .map(([key, values]) => {
-          const valuesString = Array.from(values).map(value => `'${value}'`).join(' | ')
-          return [key, valuesString]
-        }),
+        .map(
+          ([key, values]) => {
+            const valuesString = Array.from(values).map(value => `'${value}'`).join(' | ')
+            return [key, valuesString]
+          },
+        ),
     )
 
-    const interfacePropsBody = Object.entries(types).map(([key, value]) => `${key}: ${value}`).join(`\n${indent}`)
+    const interfacePropsBody = Object.entries(types)
+      .map(
+        ([key, value]) => `${key}: ${value}`,
+      )
+      .join(`\n${indent}`)
+
+    const computedProperties: ComputedProperties = Object.fromEntries(
+      Object.entries(permutationCollection)
+        .map(([key, value]) => {
+          const namesMap = Object.fromEntries(
+            Array.from(value)
+              .map((permutationValue) => {
+                const name = `is${key[0].toUpperCase()}${key.slice(1)}${permutationValue[0].toUpperCase()}${permutationValue.slice(1)}`
+                return [permutationValue, name]
+              }),
+          )
+          return [key, namesMap]
+        }),
+    ) as ComputedProperties
+
+    const computedPropertyNames = Object.keys(types)
+
+    const computedPropertiesBodies = Object.entries(computedProperties)
+      .flatMap(
+        ([permutationKey, permutationValueMapping]: [PermutationKey, { [p: PermutationValue]: PermutationComputedPropertyName }]) => {
+          return Object.entries(permutationValueMapping)
+            .map(
+              ([permutationValue, computedPropertyName]) => {
+                return `const ${computedPropertyName} = computed(() => ${permutationKey}.value === '${permutationValue}')`
+              },
+            )
+        },
+      )
 
     const code = `
 <script setup lang="ts">
+  import { computed, toRefs } from 'vue'
+
   interface Props {
     ${interfacePropsBody}
   }
 
-  defineProps<Props>()
+  const { ${computedPropertyNames.join(', ')} } = toRefs(defineProps<Props>())
+  
+  ${computedPropertiesBodies.join(`\n${createIndent(1)}`)}
 </script>
     `
 
