8670942e4f7d644af0cbd469a50f898d4e209024
mfranken <miguelfranken90@gmail.com>
2024-02-28 16:55:19 +0100

    Increase padding to bottom

--- packages/ui/src/layout.vue
+++ packages/ui/src/layout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-screen h-full px-4 bg-$figma-color-bg color-$figma-color-text py-16 -mt-16">
+  <div class="w-screen h-full px-4 bg-$figma-color-bg color-$figma-color-text pt-16 pb-24 -mt-16">
     <slot />
   </div>
 </template>
