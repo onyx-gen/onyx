5c832dd4c8fd169e09b2714b16336a35312efbfa
mfranken <miguelfranken90@gmail.com>
2024-03-01 16:51:14 +0100

    Add inference unit tests

--- packages/code/test/inference.test.ts
+++ packages/code/test/inference.test.ts
@@ -1,6 +1,8 @@
 import { describe, expect, it } from 'vitest'
 import { findNearestColor } from '../src/builder/inference/color'
 import { colorMap } from '../src/config/colors'
+import { findNearestDimension } from '../src/builder/inference/dimension'
+import { dimensionMap } from '../src/config/dimension-map'
 
 describe('colors', () => {
   describe('findNearestColor', () => {
@@ -12,6 +14,18 @@ describe('colors', () => {
     it('should handle exact match', () => {
       const result = findNearestColor({ r: 226, g: 232, b: 240 }, colorMap) // An exact color from the map
       expect(result).toBe('slate-200')
+    })
+  })
+
+  describe('findNearestDimension', () => {
+    it('should return closest dimension in the dimensionMap for provided pixel value', () => {
+      const result = findNearestDimension(19, dimensionMap) // A pixel value that is not in the map
+      expect(result).toBe('5')
+    })
+
+    it('should handle exact match', () => {
+      const result = findNearestDimension(16, dimensionMap) // An exact pixel value from the map
+      expect(result).toBe('4')
     })
   })
 })
