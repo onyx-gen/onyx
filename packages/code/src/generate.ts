2731fff842e7423c4f460818f14ea221798de1bb
mfranken <miguelfranken90@gmail.com>
2024-03-04 14:55:06 +0100

    Add try/catch around generate method

--- packages/code/src/generate.ts
+++ packages/code/src/generate.ts
@@ -11,74 +11,80 @@ import { getComponentProperties } from './set/utils'
  * @returns A Promise that resolves to the generated HTML code as a string or void if no node is selected.
  */
 export default async function generate(): Promise<string | void> {
-  const nodes = getSelectedNodes()
+  try {
+    const nodes = getSelectedNodes()
 
-  // Early return if no node is selected
-  if (nodes.length === 0) {
-    sendUnselectedMessage()
-    return
-  }
+    // Early return if no node is selected
+    if (nodes.length === 0) {
+      sendUnselectedMessage()
+      return
+    }
 
-  const parser = new FigmaNodeParser()
-  const generator = new HTMLGenerator()
+    const parser = new FigmaNodeParser()
+    const generator = new HTMLGenerator()
 
-  let html = ''
+    let html = ''
 
-  if (nodes.length === 1) {
-    const node = nodes[0]
+    if (nodes.length === 1) {
+      const node = nodes[0]
 
-    if (node.type === 'COMPONENT_SET') {
-      const componentSetProcessor = new ComponentSetProcessor()
+      if (node.type === 'COMPONENT_SET') {
+        const componentSetProcessor = new ComponentSetProcessor()
 
-      try {
-        html = await componentSetProcessor.process(node)
+        try {
+          html = await componentSetProcessor.process(node)
+        }
+        catch (error) {
+          console.error(`[Onyx Plugin] Error during component set processing`, error)
+          figma.notify('Error during component set processing')
+        }
       }
-      catch (error) {
-        console.error(`[Onyx Plugin] Error during component set processing`, error)
-        figma.notify('Error during component set processing')
+      else {
+        const tree = await parser.parse(node)
+
+        if (tree) {
+          html = generator.generate(tree)
+        }
+        else {
+          console.error('It was not possible to generate HTML code for the selected node.')
+          figma.notify('Error during HTML generation')
+        }
       }
     }
     else {
-      const tree = await parser.parse(node)
+      const componentSetProcessor = new ComponentSetProcessor()
 
-      if (tree) {
-        html = generator.generate(tree)
+      try {
+        html = await componentSetProcessor.process(nodes)
       }
-      else {
-        console.error('It was not possible to generate HTML code for the selected node.')
-        figma.notify('Error during HTML generation')
+      catch (error) {
+        console.error(`[Onyx Plugin] Error during multiple selected nodes processing`, error)
+        figma.notify('Error during multiple selected nodes processing')
       }
     }
-  }
-  else {
-    const componentSetProcessor = new ComponentSetProcessor()
 
-    try {
-      html = await componentSetProcessor.process(nodes)
-    }
-    catch (error) {
-      console.error(`[Onyx Plugin] Error during multiple selected nodes processing`, error)
-      figma.notify('Error during multiple selected nodes processing')
-    }
-  }
-
-  if (nodes.length === 1 && nodes[0].type === 'COMPONENT_SET') {
-    const componentSet = nodes[0] as ComponentSetNode
-    const componentNodes = componentSet.children as ComponentNode[]
-    sendSelectedMessage(componentNodes.map(node => ({ id: node.id, props: getComponentProperties(node) })))
-  }
-  else {
-    const componentNodes = nodes.filter(node => node.type === 'COMPONENT') as ComponentNode[]
-
-    if (componentNodes.length > 0)
+    if (nodes.length === 1 && nodes[0].type === 'COMPONENT_SET') {
+      const componentSet = nodes[0] as ComponentSetNode
+      const componentNodes = componentSet.children as ComponentNode[]
       sendSelectedMessage(componentNodes.map(node => ({ id: node.id, props: getComponentProperties(node) })))
-    else
-      sendSelectedMessage([])
-  }
+    }
+    else {
+      const componentNodes = nodes.filter(node => node.type === 'COMPONENT') as ComponentNode[]
 
-  // only send message if html is not empty
-  if (html)
-    sendHtmlMessage(html)
-  else
-    sendUnselectedMessage()
+      if (componentNodes.length > 0)
+        sendSelectedMessage(componentNodes.map(node => ({ id: node.id, props: getComponentProperties(node) })))
+      else
+        sendSelectedMessage([])
+    }
+
+    // only send message if html is not empty
+    if (html)
+      sendHtmlMessage(html)
+    else
+      sendUnselectedMessage()
+  }
+  catch (e) {
+    figma.notify('Onyx: Unexpected Error')
+    console.error(`[Onyx Plugin] Error during HTML generation`, e)
+  }
 }
