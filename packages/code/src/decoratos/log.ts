86efb268609ca39e7e3e93dc6b12ecc5f44386e8
mfranken <miguelfranken90@gmail.com>
2024-03-14 09:32:40 +0100

    Show param names when logging with decorator

--- packages/code/src/decoratos/log.ts
+++ packages/code/src/decoratos/log.ts
@@ -12,14 +12,35 @@ export function Log<T extends Constructor>(constructor: T) {
       super(...args)
       const className = constructor.name
       Object.getOwnPropertyNames(constructor.prototype).forEach((methodName) => {
-        if (typeof this[methodName] === 'function') {
+        if (methodName !== 'constructor' && typeof this[methodName] === 'function') {
           const originalMethod = this[methodName] as Function
+          const paramNames = getParamNames(originalMethod)
+
           this[methodName] = (...methodArgs: any[]) => {
-            consola.withDefaults({ level: 5 }).withTag(className).debug(methodName, methodArgs)
+            const argsWithNames = paramNames.reduce((obj, name, index) => {
+              obj[name] = methodArgs[index]
+              return obj
+            }, {} as Record<string, any>)
+
+            consola.withDefaults({ level: 5 }).withTag(className).debug(methodName, argsWithNames)
+
             return originalMethod.apply(this, methodArgs)
           }
         }
       })
     }
   }
+}
+
+/**
+ * Extracts parameter names from a function.
+ * @param func The function to extract parameter names from.
+ * @returns An array of parameter names.
+ */
+function getParamNames(func: Function): string[] {
+  const result = func.toString().match(/constructor\(\)|function\s.*?\(([^)]*)\)/)
+  if (result && result[1])
+    return result[1].replace(/ /g, '').split(',')
+
+  return []
 }
