89dc76636e15ec56c4b44620f7998673c4756585
miguelfranken <miguel.franken@rwth-aachen.de>
2024-08-21 15:37:16 +0200

    Revert "fix: remove multiplication with 16 in remToUnitless"
    This reverts commit 82fa5a48fc6bfd94f88a46ff3ca9ca3a7f843222.

--- packages/code/src/config/dimension.ts
+++ packages/code/src/config/dimension.ts
@@ -41,7 +41,7 @@ const values: number[] = [
 ]
 
 function remToUnitless(r: string): number {
-  return Number.parseFloat(round(Number.parseFloat(r.replace('rem', ''))))
+  return Number.parseFloat(round(Number.parseFloat(r.replace('rem', '')) * 16))
 }
 
 function createComputedDimensions() {
